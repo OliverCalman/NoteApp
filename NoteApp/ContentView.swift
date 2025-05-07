4085a85b910a0e49cb641eed67866aba4a4db14e
Oliver Calman <olivercalman@icloud.com>
2025-05-07 22:27:55 +1000

    Initial Commit

---
+++ NoteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoteApp
+//
+//  Created by Oliver Calman on 7/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
